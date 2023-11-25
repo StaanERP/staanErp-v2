29809ae37ead8d34a7b0488c406f1ae331db1282
Jegathish.E <Jegathish.E@stdomain.com>
2023-11-25 12:18:28 +0530

    login page update

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -7,7 +7,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import  staanlogo from "../../img/logo-final.png"
 import "./login.css"
 import DataContext from '../../context/ItemMasterContext';
-
+import Avatar from 'react-avatar';
 
 const Login = () => {
   const {login, logout, userName}= useContext(DataContext)
@@ -66,13 +66,31 @@ const Login = () => {
 }
   return (
     <>
-    
+     <ToastContainer/>
     <div className='top_div shadow-lg px-4 py-4  container-sm    loginDiv container-lg  m-auto justify-content-end '>
          
          <div className='header'>
-         <img src={staanlogo} className="img-thumbnail" alt="staan logo"/>
-         <ToastContainer/>
-         <h3 className="title pt-md-4">{userName ?  "Hi  " + userName : "Need To Login"}</h3>
+          
+        
+          <div className="row">
+            {/* <div className="col-3 p-0">
+            <img src={staanlogo} className="img-thumbnail m-0" alt="staan logo"/>
+            </div> */}
+            <div className="col-12 pe-5">
+            <Avatar size="100" facebook-id="invalidfacebookusername" src="https://graph.facebook.com/100008343750912/picture?width=100&height=100" round={true} />
+            </div>
+            <div className="col-12 ps-5 ms-1">
+            <h5 className="title nowrap pt-md-4">{userName ?  "Hi  " + userName : "Need To Login"}</h5>
+            </div>
+         </div>
+         
+         
+         <div>
+         
+         
+        
+         </div>
+         
          </div>
          <div className="row">
           <div className="col-12 text-center">
